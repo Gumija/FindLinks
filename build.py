49957562187c6570c7deafe5090070dbd843aeba
Milan Gulyas <gumija6@gmail.com>
2019-09-18 22:39:53 +0200

    Python build

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 
 import subprocess
+from pip._internal import main as pip
 
-# subprocess.run(["cd", "Rust", "&&", "cargo", "run"])
+pip(['install', 'selectolax'])
+pip(['install', 'lxml'])
+
 subprocess.run("cargo build --release", cwd="Rust", shell=True)
 subprocess.run("dotnet build -c Release", cwd="C#", shell=True)
